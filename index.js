d444980759b25cad3a78e09e4bb5dfc7b46204a5
momaaty125 <abm257349@gmail.com>
2024-11-22 15:17:56 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var mp=[
+    "Be yourself; everyone else is already taken",
+    "So many books, so little time","A room without books is like a body without a soul"
+]
+var myrandom=Math.floor(Math.random()*mp.length)
+    function mo2(){
+        console.log(mp[myrandom])
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
